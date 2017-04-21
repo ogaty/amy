e24c76584e48016c69548a79e9409c09189c9738
natasha <->
2017-04-22 01:30:43 +0900

    :coffee:service/provider/auth

---
+++ resources/views/home.blade.php
@@ -0,0 +1,34 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="list">
+    <div class="list-header">
+        <span>humberger</span>
+        <div>search-box</div>
+        <span>search-icon</span>
+    </div>
+    <div class="list-body">
+        <div>
+            <span>bell</span>
+        </div>
+        <div>
+            <ul></ul>
+        </div>
+    </div>
+    </div>
+    <div class="main">
+    <div class="row">
+        <div class="col-md-8 col-md-offset-2">
+            <div class="panel panel-default">
+                <div class="panel-heading">Dashboard</div>
+
+                <div class="panel-body">
+                    You are logged in!
+                </div>
+            </div>
+        </div>
+    </div>
+    </div>
+</div>
+@endsection
